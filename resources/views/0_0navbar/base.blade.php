642efe5505fdd5f38c19bea9adff42260ae26b02
nursyamsiana <nursabana12@gmail.com>
2023-11-25 12:01:12 +0700

    first commit

---
+++ resources/views/0_0navbar/base.blade.php
@@ -0,0 +1,33 @@
+<DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width", initial-scale="0.1">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="assets/style_navbar.css" rel="stylesheet">
+    <title>SurabayaDarurat</title>
+</head>
+<body>
+
+<header>
+    <label class="logo"><a href="/"><span style="font-family: 'Philosopher', sans-serif; font-size: 25px; font-weight: lighter;">Surabaya</span><span style="font-family: 'Big Shoulders Stencil Text', sans-serif; font-size: 28px; color: #DA1212">Darurat</span></a></label>
+    <nav>
+    <u1 class="nav_fitur">
+        <li class="dropdown">
+            <a href="javascript:void(0)" class="dropbtn">Kategori</a>
+            <div class="dropdown-content">
+                <a href="#">Kejadian Kejahatan</a>
+                <a href="#">Bencana Alam</a>
+                <a href="#">Kecelakaan Lantas</a>
+            </div>
+        </li>
+        <li><a href="#">Lapor</a></li>
+        <li><a href="#">Notifikasi</a></li>
+        <li><a href="#">Profil</a></li>
+    </u1>
+</nav>
+</header>
+
+@yield('content')
+</body>
+</html>
---
+++ resources/views/uswa/status_verif.blade.php
@@ -0,0 +1,7 @@
+@extends('0_0navbar.base')
+@section('content')
+
+<!-- MASUKIN HTML KALIAN DI BAWAH KOMEN INI -->
+
+
+@stop
