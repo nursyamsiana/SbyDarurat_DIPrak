db1b8d69741ee7d835bf6e670878fa0048ddd3c7
Uswatun Nurjanah <130342016+uswa193@users.noreply.github.com>
2023-12-03 18:45:48 +0700

    Update lapor3_upload2.blade.php

--- resources/views/uswa/lapor3_upload2.blade.php
+++ resources/views/uswa/lapor3_upload2.blade.php
@@ -7,7 +7,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="styles_uswa.css">
+    <link rel="stylesheet" href="assets/styles_uswa.css">
     <title>Lapor</title>
 </head>
 <body>
