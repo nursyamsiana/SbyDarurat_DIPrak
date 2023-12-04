9bd0d6ba9dccaaefa490db9a41ad34099ee35229
lavita8 <lavitaeliana8@gmail.com>
2023-12-04 12:07:28 +0700

    vita
    ini dashboard & page 1 ~

--- resources/views/vita/home.blade.php
+++ resources/views/vita/home.blade.php
@@ -2,6 +2,63 @@
 @section('content')
 
 <!-- MASUKIN HTML KALIAN DI BAWAH KOMEN INI -->
-
-
+<!DOCTYPE html>
+<html>
+  <head>
+    <link rel="stylesheet" href="globals.css" />
+    <link rel="stylesheet" href="style.css" />
+  </head>
+  <body>
+    <div class="dashboard">
+      <div class="div">
+        <button class="button">
+          <div class="overlap-group">
+            <img class="magnifier" src="img/magnifier-2.svg" />
+            <div class="text-wrapper">cari peristiwa</div>
+          </div>
+        </button>
+        <p class="p">
+          Laporkan dan bagikan informasi yang Anda miliki, termasuk peristiwa bencana maupun kejadian kejahatan yang
+          terjadi di wilayah Surabaya. Tetap mawas, tetap waras.
+        </p>
+        <div class="text-wrapper-2">Welcome Back, User123!</div>
+        <img class="line" src="img/line-17.svg" />
+        <div class="gambar">
+          <img class="screenshot" src="img/screenshot-20231009-185415-removebg-preview-2.png" />
+          <img class="img" src="img/screenshot-20231009-185415-removebg-preview.png" />
+          <img class="screenshot-2" src="img/screenshot-20231009-185415-removebg-preview.png" />
+        </div>
+        <div class="navbar">
+          <div class="overlap">
+            <div class="rectangle"></div>
+            <div class="rectangle-2"></div>
+            <img class="line-2" src="img/line-15.svg" />
+            <div class="tulisan">
+              <p class="surabaya-darurat">
+                <span class="span">Surabaya</span> <span class="text-wrapper-3">Darurat</span>
+              </p>
+              <div class="text-wrapper-4">Lapor</div>
+              <div class="kategori">
+                <div class="overlap-group-2">
+                  <div class="text-wrapper-5">Categories</div>
+                  <img class="angle-arrow-small" src="img/angle-arrow-small-down-1.svg" />
+                </div>
+              </div>
+            </div>
+            <div class="ikon">
+              <img class="magnifier-2" src="img/magnifier-1.svg" />
+              <div class="overlap-2">
+                <img class="bell" src="img/bell-1.svg" />
+                <div class="group">
+                  <div class="div-wrapper"><div class="text-wrapper-6">1</div></div>
+                </div>
+              </div>
+              <img class="layer" src="img/layer-100.png" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
 @stop
