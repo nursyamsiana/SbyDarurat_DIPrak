ae9cfae99d5f973cc8d00746fea1253aa3fa26b6
Aretha <143677984+arethaare@users.noreply.github.com>
2023-12-04 10:32:46 +0700

    Add files via upload
    ini js ama stylenya, masih berantakan juga keknyaa, nanti dirapihin lagiii

---
+++ public/assets/script_are1.js
@@ -0,0 +1,34 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const dropdowns = document.querySelectorAll('.answer');
+
+    dropdowns.forEach(dropdown => {
+        const select = dropdown.querySelector('.select-answer');
+        const caret = dropdown.querySelector('.caret');
+        const menu = dropdown.querySelector('.menu');
+        const options = dropdown.querySelectorAll('.menu li');
+
+        select.addEventListener('click', () => {
+            select.classList.toggle('select-clicked');
+            caret.classList.toggle('caret-rotate');
+            menu.classList.toggle('menu-open');
+        });
+
+        options.forEach(option => {
+            option.addEventListener('click', () => {
+                const selected = dropdown.querySelector('.dropdown');
+                selected.innerText = option.innerText;
+                select.classList.remove('select-clicked');
+                caret.classList.remove('caret-rotate');
+                menu.classList.remove('menu-open');
+                options.forEach(opt => {
+                    opt.classList.remove('active');
+                });
+                option.classList.add('active');
+            });
+        });
+    });
+});
+
+document.getElementById("next").addEventListener("click", function() {
+    window.location.href = "LayorPelaporan2\layarPelaporan2.html";
+});
