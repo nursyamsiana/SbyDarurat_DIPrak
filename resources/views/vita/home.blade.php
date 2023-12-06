fae63f05855432aa69d1786909a3da8649d0a1cb
Hananda Aisya M <heyhanna574@gmail.com>
2023-12-07 02:03:29 +0700

    fix landing page
    tulisan cari peristiwa berada di atas (harusnya di bawah)

--- resources/views/vita/home.blade.php
+++ resources/views/vita/home.blade.php
@@ -5,60 +5,36 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <link rel="stylesheet" href="globals.css" />
-    <link rel="stylesheet" href="style.css" />
+    <link rel="stylesheet" href="assets/style_vita.css" />
+    <link rel="stylesheet" href="assets/style_vita2.css" />
   </head>
   <body>
     <div class="dashboard">
       <div class="div">
-        <button class="button">
-          <div class="overlap-group">
-            <img class="magnifier" src="img/magnifier-2.svg" />
-            <div class="text-wrapper">cari peristiwa</div>
-          </div>
-        </button>
         <p class="p">
           Laporkan dan bagikan informasi yang Anda miliki, termasuk peristiwa bencana maupun kejadian kejahatan yang
           terjadi di wilayah Surabaya. Tetap mawas, tetap waras.
         </p>
+        <button class="button">
+          <div class="overlap-group">
+            <img class="magnifier" src="assets/img/magnifier-2.svg" />
+            <div class="text-wrapper">cari peristiwa</div>
+          </div>
+        </button>
         <div class="text-wrapper-2">Welcome Back, User123!</div>
-        <img class="line" src="img/line-17.svg" />
+        <img class="line" src="assets/img/line-17.svg" />
         <div class="gambar">
-          <img class="screenshot" src="img/screenshot-20231009-185415-removebg-preview-2.png" />
-          <img class="img" src="img/screenshot-20231009-185415-removebg-preview.png" />
-          <img class="screenshot-2" src="img/screenshot-20231009-185415-removebg-preview.png" />
+          <img class="screenshot" src="assets/img/screenshot-20231009-185415-removebg-preview-2.png" />
+          <img class="img" src="assets/img/screenshot-20231009-185415-removebg-preview.png" />
+          <img class="screenshot-2" src="assets/img/screenshot-20231009-185415-removebg-preview.png" />
         </div>
-        <div class="navbar">
-          <div class="overlap">
-            <div class="rectangle"></div>
-            <div class="rectangle-2"></div>
-            <img class="line-2" src="img/line-15.svg" />
-            <div class="tulisan">
-              <p class="surabaya-darurat">
-                <span class="span">Surabaya</span> <span class="text-wrapper-3">Darurat</span>
-              </p>
-              <div class="text-wrapper-4">Lapor</div>
-              <div class="kategori">
-                <div class="overlap-group-2">
-                  <div class="text-wrapper-5">Categories</div>
-                  <img class="angle-arrow-small" src="img/angle-arrow-small-down-1.svg" />
-                </div>
-              </div>
+
             </div>
             <div class="ikon">
-              <img class="magnifier-2" src="img/magnifier-1.svg" />
-              <div class="overlap-2">
-                <img class="bell" src="img/bell-1.svg" />
                 <div class="group">
-                  <div class="div-wrapper"><div class="text-wrapper-6">1</div></div>
                 </div>
               </div>
-              <img class="layer" src="img/layer-100.png" />
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+              <img class="layer" src="assets/img/layer-100.png" />
   </body>
 </html>
 @stop
